f63ced8c4b4bfbbd74e81f4a7bfc55b947d1fa8e
Nataliaa26 <natalia.acateco2025@mesacharter.org>
2025-08-11 12:59:00 -0700

    I added buttons that are now colored when being pressed. I also organized the list and bottons around.

--- easy-planning/src/App.jsx
+++ easy-planning/src/App.jsx
@@ -4,71 +4,81 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [username, setUsername] = useState("");
 
-const [username, setUsername] = useState("");
+  const [tasks, setTasks] = useState(["walk dog", "do hw", "Clean room"]);
 
-  const [tasks, setTasks] = useState(["Walk Dog ", "Do HW", "Clean Room"]);
-
-  return (
-    <>
-      {tasks.map(task => (
-        <h1>{task}</h1>
-      ))}
-    </>
-  );
+  function getUsername(e) {
+    setUsername(e.target.value);
+  }
 
   return (
     <>
-      <h1>
-        <p>Welcome To Easy Planning</p>
-      </h1>
-      <h3>
-        <p1>A Easy Way For Planning</p1>
-      </h3>
-      <h3>
-        {" "}
-        <button type="button">Menu! </button>{" "}
-      </h3>
-      <ul>
-        <li>
-          <button type="button">brainstorm </button>{" "}
-        </li>
-        <li>
+      <div className="container grid">
+        <h1>
+          <p>Welcome To Easy Planning</p>
+        </h1>
+        <h3>
+          <p1>A Easy Way For Planning</p1>
+        </h3>
+        <h3>
           {" "}
-          <button type="button">To-Do List </button>{" "}
-        </li>
-        <li>
-          {" "}
-          <button type="button"> Actions </button>{" "}
-        </li>
-        <li>
-          {" "}
-          <button type="button"> plans </button>{" "}
-        </li>
-        <h3>To-Do list </h3>
-
-        <button type="button">+ new task </button>
-
+          <button className="btn secondary-btn"  type="button">Menu </button>{" "}
+        </h3>
         <ul>
           <li>
-            <input type="checkbox" />
-            walk dog{" "}
+            <button type="button">brainstorm </button>{" "}
           </li>
           <li>
             {" "}
-            <input type="checkbox" />
-            do Hw{" "}
+            <button type="button">To-Do List </button>{" "}
           </li>
           <li>
             {" "}
-            <input type="checkbox" />
-            clean room{" "}
+            <button type="button"> Actions </button>{" "}
           </li>
-          <li> </li>
-        </ul>
-      </ul>
-      <input type="checkbox" />
+          <li>
+            {" "}
+            <button type="button"> plans </button>{" "}
+          </li>
+          <h2>To-Do list </h2>
+
+          <button type="button">+ new task </button>
+
+          <ul>
+            <li>
+              <input type="checkbox" />
+              walk dog{" "}
+            </li>
+            <li>
+              {" "}
+              <input type="checkbox" />
+              do Hw{" "}
+            </li>
+            <li>
+              {" "}
+              <input type="checkbox" />
+              clean room{" "}
+            </li>
+            <li> </li>
+          </ul>
+        </ul>{" "}
+        <input
+          placeholder="type your task here"
+          type="task"
+          onChange= {getUsername}
+/>
+
+<button className="btn primary-btn" type="button">add  task </button>
+
+
+
+        <h3>{username}</h3>
+        <input type="checkbox" />
+         {tasks.map((task) => (
+        <h3>{task}</h3>
+        ))}
+      </div>
     </>
   );
 }
